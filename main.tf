d5b5095a861d577cbed313e55f3f35a3afe9be9d
Laxmisharma23 <124757901+Laxmisharma23@users.noreply.github.com>
2023-03-28 13:52:43 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 provider "aws" {
-  region     = "us-east-1"
+  profile ="default"
+  region = "us-east-1"
 }
 
  
 
 resource "aws_instance" "laxmiterraform" {
-  ami           = "ami-00c39f71452c08778"
+  ami = "ami-00c39f71452c08778"
   instance_type = "t2.micro"
 }
